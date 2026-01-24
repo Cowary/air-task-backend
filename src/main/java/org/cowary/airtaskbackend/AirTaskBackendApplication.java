29f044964c34dad8eeb729b6f64e4459de74a653
cowary <airvary7@gmail.com>
2026-01-24 20:42:35 +0300

    init

---
+++ src/main/java/org/cowary/airtaskbackend/AirTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package org.cowary.airtaskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTaskBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirTaskBackendApplication.class, args);
+    }
+
+}
